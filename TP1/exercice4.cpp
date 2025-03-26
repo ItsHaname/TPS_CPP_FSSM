686c87a26a6f43d43e72b518cd0a99512bfedec5
a_b_Hanane_ <angelhananehanane@gmail.com>
2025-03-26 11:03:56 +0100

    Update exercice4.cpp

--- TP1/exercice4.cpp
+++ TP1/exercice4.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    int N;
+    int N(0),choix(0);// inisialiser les valeur en utulisant les constructeurs 
+    cout << "veuillez saisir un nombre entre 1 et 4" << endl;
+      cin >> N;
     while(1) {
         cout << "veuillez saisir un nombre entre 1 et 4" << endl;
 
@@ -13,27 +15,26 @@ int main() {
         cout << "2. Taper 2 pour décrémenter l’entier par 2" << endl;
         cout << "3. Taper 3 pour multiplier l’entier par 10" << endl;
         cout << "4. Taper 4 pour quitter" << endl;
-        cout << "votre choix?" << endl;
-        cin >> N;
+        cout << "votre choix  ? :" << endl;
+      
 
     
-        switch (N) {
+        switch (choix) {
             case 1:
                 N = N + 1;
-                cout << "N après incrémentation: " << N << endl; // Affiche le résultat
+                cout << "N après incrémentation: " << N << endl; 
                 break;
             case 2:
                 N = N - 2;
-                cout << "N après décrémentation: " << N << endl; // Affiche le résultat
+                cout << "N après décrémentation: " << N << endl; 
                 break;
             case 3:
                 N = N * 10;
-                cout << "N après multiplication: " << N << endl; // Affiche le résultat
+                cout << "N après multiplication: " << N << endl;
                 break;
             case 4:
                 cout << "vous avez quitté le programme" << endl;
-                return 0;  // Sortie du programme
-            default:
+                return 0;  
                 cout << "Erreur nombre saisie incorrect" << endl;
         }
     }
