9a6ecc95d8483a5e82f621312d5fcf5767b746a0
a_b_Hanane_ <angelhananehanane@gmail.com>
2025-03-26 10:36:22 +0100

    Update EX3.cpp

--- TP1/EX3.cpp
+++ TP1/EX3.cpp
@@ -1,29 +1,32 @@
 #include<iostream>
+#include<unistd.h>
+#include<unistd.h>
+#include <cstdio> // Pour sprintf
 using namespace std;
 
 int main() {
     int positif;
-    int somme = 0;  // Initialisation de somme à 0
-    float moy;  // Moyenne de type float
-    int i = 0;  // Compteur d'éléments saisis
-
-    cout << "Veuillez saisir un entier strictement positif (0 pour arrêter) : " << endl;
-
+    int somme = 0;  
+    float moy;  
+    int i = 0;  
     do {
-        cin >> positif;
+         cout << "Veuillez saisir un entier strictement positif (0 pour arrêter) : " << endl;
+          cin >> positif;
 
-        if (positif > 0) {  // On accepte uniquement les entiers strictement positifs
-            somme += positif;  // Ajouter l'entier à la somme
-            i++;  // Incrémenter le compteur
+        if (positif > 0) {  
+            somme += positif;  
+            i++;  
         } else if (positif < 0) {
             cout << "Veuillez entrer un nombre positif !" << endl;
         }
 
-    } while (positif != 0);  // La saisie continue tant que l'utilisateur ne tape pas 0
-
-    // Calcul et affichage de la moyenne si des nombres ont été saisis
-    if (i > 0) {
-        moy = float(somme) / i;  // Calcul de la moyenne
+    } while (positif != 0);  
+    if(positif==0){
+        char erreur[100];
+        int len=sprint("ERREUR",erreur);
+        write(2,len, erreur);}
+       if (i > 0) {
+        moy = float(somme) / i;  
         cout << "La moyenne des nombres saisis est : " << moy << endl;
     } else {
         cout << "Aucun nombre positif n'a été saisi." << endl;
